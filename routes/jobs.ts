3825de4be1cc27b1b1b67fa96acc4409d1fd6a9d
cgyee <cyee2017@fau.edu>
2025-02-08 20:04:52 -0500

    refactor: error handling for rest api routes

--- routes/jobs.ts
+++ routes/jobs.ts
@@ -3,24 +3,43 @@ import { removeFields, validateJob } from "../utils.ts";
 import db, { type Job } from "../db.ts";
 const router = express.Router();
 
+type ResponseMessage = { error: boolean; message: string } | Job[] | Job;
+
 router.get("/", async (req, res, next) => {
+  let status = 500;
+  let message: ResponseMessage = {
+    error: true,
+    message: "Internal server error",
+  };
   try {
     // check cache
     // if not in cache, check db
     const jobs = (await db.findAll()).map((j) => removeFields(j));
     if (jobs.length === 0) {
-      res.status(404).send({ error: true, message: "No jobs found" });
+      status = 404;
+      message = { error: true, message: "No jobs found" };
+      // res.status(404).send({ error: true, message: "No jobs found" });
+    } else {
+      // res.status(200).send(jobs);
+      status = 200;
+      message = jobs;
     }
-    res.status(200).send(jobs);
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .send({ error: true, message: `Internal server error: ${error}` });
+    // res
+    status = 500;
+    message = { error: true, message: `Internal server error: ${error}` };
+  } finally {
+    res.status(status).json(message);
   }
 });
 
 router.post("/", async (req, res, next) => {
+  let status = 500;
+  let message: ResponseMessage = {
+    error: true,
+    message: "Internal server error",
+  };
   try {
     const job = <Job>req.body;
     const [result, valid] = validateJob(
@@ -29,54 +48,75 @@ router.post("/", async (req, res, next) => {
       req.body.endDate
     );
     if (!valid) {
-      res.status(400).send(result);
+      status = 400;
+      message = { error: true, message: result };
     }
     // check cache
     // if not in cache, write to db then cache
-    const resultJob = db.findOne({ ...job });
-    if (!result) {
-      res
-        .status(400)
-        .send({ error: true, message: "Matching job eventName found" });
+    const resultJob = await db.findOne({ ...job });
+    if (resultJob == null) {
+      status = 400;
+      message = { error: true, message: "Matching job eventName found" };
+    } else {
+      status = 201;
+      message = resultJob;
     }
-    res.status(201).send(resultJob);
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .send({ error: true, message: `Internal server error: ${error}` });
+    res;
+    status = 500;
+    message = { error: true, message: `Internal server error: ${error}` };
+  } finally {
+    res.status(status).json(message);
   }
 });
 
 router.put("/:id", async (req, res, next) => {
+  let status = 500;
+  let message: ResponseMessage = {
+    error: true,
+    message: "Internal server error",
+  };
   try {
     const id = req.params.id;
-    const job = <Job>req.body;
+    const job: Job = req.body;
     const [result, valid] = validateJob(
       job,
       req.body.startDate,
       req.body.endDate
     );
     if (!valid) {
-      res.status(400).send({ error: true, message: result });
+      status = 400;
+      message = { error: true, message: result };
     }
     // check cache
     // if not in cache, check db
     // update db
     const ok = db.update({ id, eventName: "" }, { ...job });
     if (!ok) {
-      res.status(400).send({ error: true, message: "Job not found" });
+      // res.status(400).send({ error: true, message: "Job not found" });
+      status = 400;
+      message = { error: true, message: "Job not found" };
+    } else {
+      // res.status(204).send(job);
+      status = 204;
+      message = job;
     }
-    res.status(204).send(job);
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .send({ error: true, message: `Internal server error: ${error}` });
+    status = 500;
+    message = { error: true, message: `Internal server error: ${error}` };
+  } finally {
+    res.status(status).json(message);
   }
 });
 
 router.delete("/:id", async (req, res, next) => {
+  let status = 500;
+  let message: ResponseMessage = {
+    error: true,
+    message: "Internal server error",
+  };
   try {
     const id = req.params.id;
     // check cache
@@ -84,14 +124,18 @@ router.delete("/:id", async (req, res, next) => {
     // if in db proceed to process job data
     const ok = db.remove({ id, eventName: "" });
     if (!ok) {
-      res.status(400).send({ error: true, message: "Job not found" });
+      status = 400;
+      message = { error: true, message: "Job not found" };
+    } else {
+      status = 204;
+      message = { error: false, message: "Job deleted" };
     }
-    res.status(204).send("Job deleted");
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .send({ error: true, message: `Internal server error: ${error}` });
+    status = 500;
+    message = { error: true, message: `Internal server error: ${error}` };
+  } finally {
+    res.status(status).json(message);
   }
 });
 
