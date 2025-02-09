45b09b1d0a44937609a8fe066b745c59de940799
cgyee <cyee2017@fau.edu>
2025-02-08 19:31:19 -0500

    feat: fixed missing listen on server

--- app.ts
+++ app.ts
@@ -1,14 +1,17 @@
 // import createError from "http-errors";
-import path from "path";
 // import cookieParser from "cookie-parser";
 import express from "express";
 
-import jobsRouter from "./routes/jobs";
+import jobsRouter from "./routes/jobs.ts";
 
+const port = 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/jobs", jobsRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
