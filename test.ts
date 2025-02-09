50bd787c7656d1c2c9e88f0ead315949859b55ce
cgyee <cyee2017@fau.edu>
2025-02-08 19:30:06 -0500

    feat: added test for db logic

--- test.ts
+++ test.ts
@@ -1,24 +1,98 @@
-// Create a test file for db.ts getJob function
+import db, { Job } from "./db";
 import { describe, it, expect } from "@jest/globals";
-import { find } from "./db";
+
+const staticData = {
+  eventName: "Morning Scene",
+  startDate: "2024-01-01",
+
+  endDate: "2024-01-31",
+  recurrence: {
+    interval: 1,
+    frequency: "day",
+  },
+} as Job;
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+describe("create job", () => {
+  const data = staticData;
+  it(`should return true for ${data.eventName}`, async () => {
+    const ok = await db.create({ ...data });
+    expect(ok).toBe(true);
+  });
+});
 
 describe("find job", () => {
-  it("should return a job object", () => {
-    const [job, found] = find("test");
-    expect(job).toEqual({
-      eventName: "test",
-      startDate: "2021-10-10",
-      endDate: "2021-10-10",
-      recurrence: {
-        interval: 1,
-        frequency: "daily",
-      },
-    });
-    expect(found).toBe(true);
+  const data = staticData;
+  const dummyData = {
+    eventName: "Test Scene",
+    startDate: "2024-01-01",
+
+    endDate: "2024-01-31",
+    recurrence: {
+      interval: 1,
+      frequency: "day",
+    },
+  } as Job;
+  it('should return a job null for "Test Scene"', async () => {
+    const job = await db.findOne({ ...dummyData });
+    expect(job).toEqual(null);
   });
-  it("should return null if job is not found", () => {
-    const [job, found] = find("test2");
-    expect(job).toBe(null);
-    expect(found).toBe(false);
+  it('should return a job object for "Morning Scence"', async () => {
+    const job = await db.findOne({ ...data });
+    expect(job?.eventName).toBe(data.eventName);
+  });
+});
+
+describe("update job", () => {
+  const data = staticData;
+  const dummyData = {
+    eventName: "Test Scene",
+    startDate: "2024-01-01",
+
+    endDate: "2024-01-31",
+    recurrence: {
+      interval: 2,
+      frequency: "day",
+    },
+  } as Job;
+  describe(`updating ${data.eventName}`, () => {
+    it(`should return true for ${data.eventName}`, async () => {
+      const ok = await db.update({ ...data }, dummyData);
+      expect(ok).toBe(true);
+    });
+  });
+  describe("checking update", () => {
+    it(`should return false for ${data.eventName}`, async () => {
+      await sleep(100);
+      const job = await db.findOne({ ...data });
+      expect(job).toBe(null);
+    });
+  });
+  describe(`finding updated job ${dummyData.eventName}`, () => {
+    it(`should return a job object for ${dummyData.eventName}`, async () => {
+      await sleep(100);
+      const job = await db.findOne({ ...dummyData });
+      expect(job?.eventName).toBe(dummyData.eventName);
+    });
+  });
+});
+
+describe("remove a job", () => {
+  const data = {
+    eventName: "Test Scene",
+    startDate: "2024-01-01",
+
+    endDate: "2024-01-31",
+    recurrence: {
+      interval: 1,
+      frequency: "day",
+    },
+  } as Job;
+  it(`should return true for ${data.eventName}`, async () => {
+    const ok = await db.remove({ ...data });
+    expect(ok).toBe(true);
   });
 });
